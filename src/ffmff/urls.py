4d6f207d19412678de2a49db283081e059ea3cf3
Steffen Neubauer <stefreak@stefreak.de>
2010-02-18 20:52:15 +0100

    Debug mode: serves /media/ files now completely by itself

--- src/ffmff/urls.py
+++ src/ffmff/urls.py
@@ -1,14 +1,26 @@
 from django.conf.urls.defaults import *
-
-from django.contrib import admin
-admin.autodiscover()
+from settings import DEBUG
 
 urlpatterns = patterns('',
 	(r'^events/', include('ffmff.events.urls')),
-	(r'^admin/(.*)', admin.site.root),
 )
 
 # Home
 urlpatterns += patterns('django.views.generic.simple',
 	(r'^$', 'direct_to_template', {'template': 'home.html'}),
 )
+
+if DEBUG:
+	# serve static files in debug mode
+	from settings import MEDIA_URL, MEDIA_ROOT
+	urlpatterns += patterns('django.views.static',
+		(r'^%s(?P<path>.*)$' % MEDIA_URL[1:], 'serve', 
+		        {'document_root': MEDIA_ROOT, 'show_indexes': True}),
+	)
+
+	# admin page
+	from django.contrib import admin
+	admin.autodiscover()
+	urlpatterns += patterns('',
+		(r'^admin/(.*)', admin.site.root),
+	)
