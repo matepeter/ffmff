c381554e8412f75f4db8ae4089d138e6f97ec8cb
Steffen Neubauer <stefreak@stefreak.de>
2010-02-19 00:39:26 +0100

    events: added submit_event view + updated urls.py

--- src/ffmff/events/urls.py
+++ src/ffmff/events/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.defaults import *
 from models import Event, Tag
+from django.views.generic.simple import direct_to_template
 
 from django.contrib import admin
 admin.site.register(Event)
 admin.site.register(Tag)
 
 urlpatterns = patterns('ffmff.events.views',
+	(r'^submit/$', 'submit_event'),
+	(r'^submit/success/$', direct_to_template, {'template': 'events/submit_success.html'}),
 	(r'^(?P<id>\d)/$', 'view_event'),
 )
--- src/ffmff/events/views.py
+++ src/ffmff/events/views.py
@@ -1,4 +1,5 @@
 from models import Event, Tag
+from forms import EventForm
 from django.http import Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
@@ -11,4 +12,22 @@ def view_event(request, id):
 	
 	return render_to_response('events/view_event.html',
 	                          { 'event': event },
+	                          context_instance=RequestContext(request))
+
+def submit_event(request):
+	if request.method == 'POST':
+		form = EventForm(request.POST)
+		if form.is_valid():
+			event = Event()
+			event.name = form.cleaned_data['name']
+			event.desc = form.cleaned_data['desc']
+			event.url = form.cleaned_data['url']
+			event.published = False
+			event.save()
+			return HttpResponseRedirect('/events/submit/success/')
+	else:
+		form = EventForm()
+	
+	return render_to_response('events/submit_event.html',
+	                          { 'form': form },
 	                          context_instance=RequestContext(request))
