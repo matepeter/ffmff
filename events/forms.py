e0c5a68b59064c1f1252faabe475022492246306
Steffen Neubauer <stefreak@stefreak.de>
2011-05-26 22:56:59 +0200

    added jquery-ui datepicker for date selection in submit page

--- events/forms.py
+++ events/forms.py
@@ -10,5 +10,5 @@ class EventForm(forms.Form):
 	name = forms.CharField(max_length=50)
 	desc = forms.CharField(max_length=5000, widget=forms.Textarea)
 	url  = forms.URLField(max_length=100)
-	date_start = forms.DateField()
-	date_end   = forms.DateField()
+	date_start = forms.DateField(widget=forms.TextInput(attrs={'class':'datepicker'}))
+	date_end   = forms.DateField(widget=forms.TextInput(attrs={'class':'datepicker'}))
