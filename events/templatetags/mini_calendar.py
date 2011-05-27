27e715b052bd71f12a8c9e5e3b65fef83634dca2
Steffen Neubauer <stefreak@stefreak.de>
2011-05-27 12:27:13 +0200

    different mini_calendar color for past events

--- events/templatetags/mini_calendar.py
+++ events/templatetags/mini_calendar.py
@@ -1,6 +1,9 @@
 from django import template
+from datetime import datetime
+
 register = template.Library()
 
 @register.inclusion_tag('events/mini_calendar.html')
 def mini_calendar(date):
-	return {'date':date}
+	past = date < datetime.now().date()
+	return {'date': date, 'past': past}
