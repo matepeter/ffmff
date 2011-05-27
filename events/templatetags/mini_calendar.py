2e05768c6580984182c76b4b63c39e277a2e721e
Steffen Neubauer <stefreak@stefreak.de>
2011-05-27 21:39:25 +0200

    implemented mini_calendar hover end date

--- events/templatetags/mini_calendar.py
+++ events/templatetags/mini_calendar.py
@@ -4,6 +4,10 @@ from datetime import datetime
 register = template.Library()
 
 @register.inclusion_tag('events/mini_calendar.html')
-def mini_calendar(date):
-	past = date < datetime.now().date()
-	return {'date': date, 'past': past}
+def mini_calendar(date_begin, date_end):
+	past = date_end < datetime.now().date()
+	return {
+		'date_begin': date_begin,
+		'date_end': date_end,
+		'past': past
+	}
